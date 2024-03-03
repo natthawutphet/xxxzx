7482e6e9b3dbc33ef4eb3e2bc23e2233ac3530df
xxx <xnatthawut1990@gmail.com>
2024-03-03 10:31:04 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+import "@/styles/globals.css";
+import '@/styles/style.css'
+import '@/styles/style1.css'
+import '@/styles/style3.css'
 import { AppProps } from 'next/app';
 import Script from 'next/script';
 
