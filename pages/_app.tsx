dfd51c28a64d327fc1f6395a6a655dffa4542a70
xxx <xnatthawut1990@gmail.com>
2024-03-03 10:17:51 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,28 @@ import '@/styles/style3.css'
 
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import Script from 'next/script';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16457513085"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16457513085');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
 
