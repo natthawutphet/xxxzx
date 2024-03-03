3ace6f29536194441e098c24736db3b71e2b57aa
xxx <xnatthawut1990@gmail.com>
2024-03-03 10:04:39 +0700

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,13 +11,17 @@ export default function Document() {
     
     <Head>
   
-    <script dangerouslySetInnerHTML={{ 
-  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5DF4CHLK');` 
-}} />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16457513085"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16457513085');
+              `,
+            }}
+          />
 
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,11 @@ export default function index() {
            <meta name="keywords" content="ยิงads,facebook,สายเทา,โฆษณา,ยิงads facebook สายเทา,รับยิงads,รับยิงแอด สายเทา,facebook ads,google ads,google,รับยิงแอด,ads,รับทำโฆษณา,รับโฆษณา,Facebook,การตลาด,โฆษณาออนไลน์,เว็บไซต์,ตลาดเป้าหมาย,โฆษณาบนโซเชียลมีเดีย,Google Ads,การโฆษณาบน Facebook,การโฆษณาออนไลน์บนสื่อต่าง ๆ,การตลาดออนไลน์,การโปรโมท,โฆษณา Facebook,การโฆษณา Google,การโฆษณาสินค้า,การโฆษณาโปรโมชั่น,วิธีการโฆษณา,ความสำเร็จในการตลาด,บริการโฆษณา,การตลาดออนไลน์บน Facebook,การโฆษณาบนเว็บ,การวางแผนโฆษณา,รับจ้างโฆษณา"/>
            <meta name="robots" content="index, follow"/>
            <meta name="author" content="adsmanager"/>
-           <meta property="og:title" content="ชื่อหน้าเว็บไซต์สำหรับการแชร์"/>
+           <meta property="og:title" content="รับทำโฆษณาออนไลน์สายเทา"/>
            <meta property="og:description" content="รับทำโฆษณาออนไลน์สายเทา Google, YouTube, Facebook"/>
-           <meta property="og:image" content="https://www.servermanagerads.com/img/ads.jpg"/>
+           <meta property="og:image" content="https://www.servermanagerads.com/favicon.ico"/>
            <link rel="canonical" href="https://www.servermanagerads.com/"/>
+           
 
     </Head>
     
