28de376efe3ccda52fcb335507f9167e9b2c38cd
เอ็ก <136637894+natthawutphet@users.noreply.github.com>
2024-03-08 11:58:31 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,7 +58,12 @@ const urls = "https://www.service-ads.com";
 
         
  
+    <div className="lines">
 
+<Link href='/line' >   
+<img src="line.png" className="rounded-circle"  alt="line" />
+</Link>
+</div>
    
 
 
